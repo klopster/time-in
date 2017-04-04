d0ac67a3f963fa57a866982d94af6b946e5f8f97
klopster <klop@seznam.cz>
2017-04-04 20:20:30 +0200

    sample timein

---
+++ app/admin/dashboard.rb
@@ -0,0 +1,31 @@
+ActiveAdmin.register_page "Dashboard" do
+
+  menu priority: 1, label: proc{ I18n.t("active_admin.dashboard") }
+
+  content title: proc{ I18n.t("active_admin.dashboard") } do
+  columns do
+  column do  
+	panel "Your tasks for this week" do
+    table_for current_admin_user.tasks.where('due_date > ? and due_date < ?', Time.now, 1.week.from_now) do |t|
+      t.column("Status") { |task| status_tag (task.is_done ? "Done" : "Pending"), (task.is_done ? :ok : :error) }
+      t.column("Title") { |task| link_to task.title, admin_task_path(task) }
+      t.column("Assigned To") { |task| task.admin_user.email }
+      t.column("Due Date") { |task| task.due_date? ? l(task.due_date, :format => :long) : '-' }
+  	  end
+  	end
+ 	end
+ 	column do
+  panel "Tasks that are late" do
+    table_for current_admin_user.tasks.where('due_date < ?', Time.now) do |t|
+      t.column("Status") { |task| status_tag (task.is_done ? "Done" : "Pending"), (task.is_done ? :ok : :error) }
+      t.column("Title") { |task| link_to task.title, admin_task_path(task) }
+      t.column("Assigned To") { |task| task.admin_user.email }
+      t.column("Due Date") { |task| task.due_date? ? l(task.due_date, :format => :long) : '-' }
+    	end
+  	end
+  end
+    
+  end
+  end
+  
+end
